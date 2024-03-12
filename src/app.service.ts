6a263c2f5e1aed69510a4330aa2f604defb773d1
Steven Castro <castrostevenyc@gmail.com>
2024-03-11 22:53:11 -0400

    Added new model

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { Test } from '@prisma/client';
 
 @Injectable()
 export class AppService {
   constructor(private prisma: PrismaService) {}
 
-  async getHello(): Promise<Test[]> {
+  async getHello() {
     try {
       const data = await this.prisma.test.findMany();
-      return data;
+      return JSON.stringify(data);
     } catch (error) {
       throw error;
     }
